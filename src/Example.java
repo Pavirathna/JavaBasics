42ac842fd2ba5951c5f3bdac1096a525b24c00c1
pavirathna <pvthra027@gmail.com>
2019-05-19 22:57:55 +0530

    Examples of creating variables

---
+++ src/Example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String [] args)    {
+        System.out.println("Variables");
+        Scanner sc=new Scanner(System.in);
+        String username=sc.next();
+        System.out.println("Hi....."+username);
+
+    }
+}
